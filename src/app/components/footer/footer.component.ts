21064cb7b1b4a777b76dd4fb459bf23b085ccb0b
Hossein Mansouri <hossein.mansouri@medis.land>
2023-10-09 11:40:17 +0330

    create voice selector designs

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  StatusMode: string = "default";
+  // inputs display
+  displayMessageInput: boolean = true;
+  displayVoiceSelector: boolean = false;
+  // buttons display
+  displaySendMessageButton: boolean = false;
+  displayVoiceChangerButton: boolean = true;
+  displayBackButton: boolean = false;
+  // Inputs
+  @Input() MessageInput: string = "";
+  @Input() VoiceSelector: string = "";
+
+  setDefaultMode() {
+    this.displayMessageInput = true;
+    this.displayVoiceSelector = false;
+    this.displaySendMessageButton = false;
+    this.displayVoiceChangerButton = true;
+    this.displayBackButton = false;
+  }
+
+  setTypingMode(event: any) {
+    if (event.target.value === "") {
+      this.setDefaultMode();
+    } else {
+      this.displayMessageInput = true;
+      this.displayVoiceSelector = false;
+      this.displaySendMessageButton = true;
+      this.displayVoiceChangerButton = false;
+      this.displayBackButton = false;
+    }
+  }
+  setVoiceChangerMode() {
+    this.displayMessageInput = false;
+    this.displayVoiceSelector = true;
+    this.displaySendMessageButton = false;
+    this.displayVoiceChangerButton = false;
+    this.displayBackButton = true;
+  }
 }
