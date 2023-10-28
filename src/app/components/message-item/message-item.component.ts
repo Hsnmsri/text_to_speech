baaea5d037f2ef2c03698dfb2832eba297e683fb
Hossein Mansouri <hossein.mansouri@medis.land>
2023-10-28 14:39:33 +0330

    create speaker for messages

--- src/app/components/message-item/message-item.component.ts
+++ src/app/components/message-item/message-item.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MessageItemComponent implements OnInit {
 
-  @Input() message_content?: string | null = null;
+  @Input() message_content?: string;
 
   constructor() {
 
@@ -15,5 +15,20 @@ export class MessageItemComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  Speak(): void {
+    if (!('speechSynthesis' in window)) {
+      alert('TTS in this browser not supported!');
+    }
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = this.message_content ?? "content not found!";
+    const voices = window.speechSynthesis.getVoices();
+    /*
+        Select voice
+        => first voice
+    */
+    speech.voice = voices[0];
+    window.speechSynthesis.speak(speech);
   }
 }
